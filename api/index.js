794d7520e0ffecc3a1f72c0407bd2b6838193b0a
SPRHackz <sprhackz003@gmail.com>
2024-06-01 13:18:05 +0530

    add middleware &func to handle the error

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 dotenv.config()
+
 mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log("MongoDB is Connected"))
 .catch((e)=>console.log(`Error : ${e}`))
@@ -14,3 +15,12 @@ app.listen(3000,()=>console.log("Server is Running on Port 3000"))
 
 app.use("/api/user",userRoutes)
 app.use("/api/auth",authRoutes)
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
